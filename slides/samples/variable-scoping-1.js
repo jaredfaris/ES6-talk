4787efe0ddb9f83c75862f4a2919a101a6e4028b
Jared Faris <jfaris@gmail.com>
2015-08-31 10:48:13 -0400

    Added example of hoisting

---
+++ slides/samples/variable-scoping-1.js
@@ -0,0 +1,11 @@
+function variableScopringDemo1() {
+    // myText gets hoisted here
+
+    debugger
+
+    setTimeout(function(){
+        alert(myText);
+    }, 1000);
+
+    var myText = "I'm a string!";
+}
