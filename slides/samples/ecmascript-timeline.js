7b18acd5d644b53fd15992bf04d6f0cbe3028cf0
Jared Faris <jfaris@gmail.com>
2015-12-24 14:23:44 -0500

    Moved the timeline styles into SASS. Broke out the JS

---
+++ slides/samples/ecmascript-timeline.js
@@ -0,0 +1,94 @@
+"use strict"
+class timelineManager {
+    constructor(steps, timelineImageId, timelineTitleId) {
+        this.steps = steps;
+        this.timelineImage = document.getElementById(timelineImageId);
+        this.timelineTitle = document.getElementById(timelineTitleId);
+        this.counter = 0;
+        this.maxCounter = steps.length - 1; //accounts for 0 index
+
+        this.shiftTimeline = () => {
+            this.timelineImage.style.transform = `translate(${this.steps[this.counter].pos},0)`;
+
+            this.timelineTitle.innerHTML = this.steps[this.counter].name;
+        }
+
+        this.shiftTimeline();
+    }
+
+    next() {
+        if(this.counter < this.maxCounter)
+            this.counter++;
+
+        this.shiftTimeline();
+    }
+
+    prev() {
+        if(this.counter > 0)
+            this.counter--;
+
+        this.shiftTimeline();
+    }
+
+    first() {
+        this.counter = 0;
+        this.shiftTimeline();
+    }
+
+    last() {
+        this.counter = this.maxCounter;
+        this.shiftTimeline();
+    }
+}
+
+var timelineData = [
+    {
+        name: "Mocha Created",
+        pos: 0
+    },
+    {
+        name: "ECMAScript 1 Published",
+        pos: "-430px"
+    },
+    {
+        name: "ECMAScript 2 Published",
+        pos: "-750px"
+    },
+    {
+        name: "ECMASCript 3 Published",
+        pos: "-1300px"
+    },
+    {
+        name: "Firefox Released",
+        pos: "-1700px"
+    },
+    {
+        name: "Chrome Released",
+        pos: "-3370px"
+    },
+    {
+        name: "ECMAScript 5 Published",
+        pos: "-4000px"
+    },
+    {
+        name: "ECMAScript 5.1 Published",
+        pos: "-4400px"
+    },
+    {
+        name: "ECMAScript 6 Published",
+        pos: "-5460px"
+    },
+    {
+        name: "The Future",
+        pos: "-5600px"
+    }
+];
+
+
+window.onload = () => {
+    var tm = new timelineManager(timelineData, "ecmascriptTimeline", "ecmascriptTimelineTitle");
+    document.getElementById("ecmascriptTimelineFirst").onclick = () => tm.first();
+    document.getElementById("ecmascriptTimelineLeft").onclick = () => tm.prev();
+    document.getElementById("ecmascriptTimelineRight").onclick = () => tm.next();
+    document.getElementById("ecmascriptTimelineLast").onclick = () => tm.last();
+}
